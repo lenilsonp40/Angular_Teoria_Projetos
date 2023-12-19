bf398d5cbb735c3659d907dec88a949490333c7d
Lenilson Soares <lenilsonp40@gmail.com>
2023-12-18 22:12:23 -0300

    components

--- curso de Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
+++ curso de Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'meu-primeiro-projeto';
+  title = 'meu-primeiro-projeto-latest';
 }
