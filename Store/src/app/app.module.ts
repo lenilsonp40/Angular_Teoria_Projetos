8427a6ff69630e454b0bed2ded8762166b5090dc
Lenilson Soares <lenilsonp40@gmail.com>
2024-02-15 22:21:46 -0300

    store

--- Store/src/app/app.module.ts
+++ Store/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {IndexModule} from './pages/index/index.module';
-import {PortifolioModule} from './pages/portifolio/portifolio.module';
-import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    IndexModule,
-    PortifolioModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
